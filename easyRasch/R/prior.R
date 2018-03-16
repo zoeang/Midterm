a0b9c2e15489cd41fae859a9e773df69b7822252
zoeang <36051346+zoeang@users.noreply.github.com>
2018-03-16 12:16:09 -0500

    Made prior function
    This was deceptively easy... *gets anxious*

---
+++ easyRasch/R/prior.R
@@ -0,0 +1,30 @@
+#' Estimate theta_j using Bayesian prior
+#' 
+#' Estimates theta_j using Bayesian prior to get a more reasonable estimate of the parameter; this wull use the normal curve with mean=0 and sd=3 
+#' 
+#' @param theta A proposed valie of theta_j
+#' 
+#' @return The height of the specified nromal curve evaluated at theta_j
+#' @author Zoe <\email{zoeang@wustl.edu}>
+#' 
+#' @rdname probability
+#' @export
+setMethod("initialize", "numeric", function(.Object, ...){
+  value=callNextMethod()
+  validObject(value)
+  return(value)
+})
+
+#' @export
+setGeneric("prior", #The generic is called probability
+           function(object="numeric") { #***
+             standardGeneric("prior")
+           })
+
+
+#' @export
+setMethod(f="prior", signature="numeric",
+          definition=function(object=theta){
+           dnorm(object,0,3)
+          }
+)
